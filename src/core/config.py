6f2583135df4ee77ee4e561d630944dac2acd97d
Cristian <crismonsalve4@gmail.com>
2025-10-16 00:02:59 -0500

    feat(config): Cambio API key

--- src/core/config.py
+++ src/core/config.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-EPM_URL = "https://www.epm.com.co/client/interrupciones-del-servicio-de-agua"
+EPM_URL = "https://www.epm.com.co/clientesyusuarios/interrupciones-del-servicio/agua/"
